0a8e1a005fba5882be602c979b0e50284da813c0
Chase Miller <chasemillerwd@gmail.com>
2021-02-01 17:17:29 -0500

    Slot swapping works

--- src/main/java/fwb/fwborbit/common/containers/CoalGeneratorContainer.java
+++ src/main/java/fwb/fwborbit/common/containers/CoalGeneratorContainer.java
@@ -1,28 +1,28 @@
 package fwb.fwborbit.common.containers;
 
-import com.sun.org.apache.xpath.internal.operations.Or;
 import fwb.fwborbit.common.Orbit;
 import fwb.fwborbit.common.Registration;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.Slot;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IWorldPosCallable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.extensions.IForgeItem;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
 import net.minecraftforge.items.wrapper.InvWrapper;
 
 public class CoalGeneratorContainer extends Container {
-    private TileEntity tileEntity;
-    private PlayerEntity playerEntity;
-    private IItemHandler playerInventory;
+    private final TileEntity tileEntity;
+    private final PlayerEntity playerEntity;
+    private final IItemHandler playerInventory;
 
     public CoalGeneratorContainer(int windowId, World world, BlockPos pos, PlayerInventory playerInventory, PlayerEntity player) {
         super(Registration.COAL_GENERATOR_CONTAINER.get(), windowId);
@@ -35,7 +35,8 @@ public class CoalGeneratorContainer extends Container {
                 addSlot(new SlotItemHandler(h, 0,64, 124));
             });
         }
-        layoutGui(8, 84, 80, 59);
+        layoutCoalGeneratorSlots(80, 59);
+        layoutPlayerInventorySlots(8, 84);
     }
 
     @Override
@@ -52,12 +53,11 @@ public class CoalGeneratorContainer extends Container {
         return index;
     }
 
-    private int addSlotBox(IItemHandler handler, int index, int x, int y, int horAmount, int dx, int verAmount, int dy) {
+    private void addSlotBox(IItemHandler handler, int index, int x, int y, int horAmount, int dx, int verAmount, int dy) {
         for (int j = 0; j < verAmount; j++) {
             index = addSlotRange(handler, index, x, y, horAmount, dx);
             y += dy;
         }
-        return index;
     }
 
     private void layoutPlayerInventorySlots(int leftCol, int topRow) {
@@ -71,12 +71,7 @@ public class CoalGeneratorContainer extends Container {
 
     private void layoutCoalGeneratorSlots(int leftCol, int topRow) {
         // Coal generator input slot
-        addSlotBox(playerInventory, 36, leftCol, topRow, 1, 18, 1, 18);
-    }
-
-    private void layoutGui(int invLeftCol, int invTopRow, int cgLeftCol, int cgTopRow) {
-        layoutPlayerInventorySlots(invLeftCol, invTopRow);
-        layoutCoalGeneratorSlots(cgLeftCol, cgTopRow);
+        addSlot(new SlotItemHandler(playerInventory, 36, leftCol, topRow));
     }
 
     @Override
@@ -88,32 +83,30 @@ public class CoalGeneratorContainer extends Container {
             ItemStack stack = slot.getStack();
             itemStack = stack.copy();
             if (index == 0) {
-                Orbit.LOGGER.debug("Index == 0");
                 if (!this.mergeItemStack(stack, 1, 37, true)) {
-                    Orbit.LOGGER.debug("ItemStack Empty");
                     return ItemStack.EMPTY;
                 }
                 slot.onSlotChange(stack, itemStack);
             }
             else {
-                if (index < 28) {
-                    Orbit.LOGGER.debug("Index < 28 ");
-                    if (!this.mergeItemStack(stack, 27, 37, false)) {
-                        Orbit.LOGGER.debug("ItemStack Empty");
+                if (stack.getBurnTime() > 0) {
+                    if (!this.mergeItemStack(stack, 0, 1, false)) {
                         return ItemStack.EMPTY;
                     }
                 }
-                else if (index < 37 && !this.mergeItemStack(stack, 0, 28, false)) {
-                    Orbit.LOGGER.debug("Index < 37 and ItemStack Empty");
+                else if (index < 28) {
+                    if (!this.mergeItemStack(stack, 28, 37, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                }
+                else if (index < 37 && !this.mergeItemStack(stack, 1, 28, false)) {
                     return ItemStack.EMPTY;
                 }
             }
             if (stack.isEmpty()) {
-                Orbit.LOGGER.debug("Stack is empty");
                 slot.putStack(ItemStack.EMPTY);
             }
             else {
-                Orbit.LOGGER.debug("Stack changed");
                 slot.onSlotChanged();
             }
             if (stack.getCount() == itemStack.getCount()) {
